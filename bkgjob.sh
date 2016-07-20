88f35431c5e3554a7c62ee7ef465b9b6d2e7a4c2
lum4chi <lum4chi@gmail.com>
2016-07-20 17:49:26 +0200

    Initial commit

---
+++ bkgjob.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2016 Francesco Lumachi <francesco.lumachi@gmail.com>
+cmd="$@"        # Command to launch in background with args
+
+# If "sudo" is used for 1st time, need to call a dummy sudo (if not, sudo runned
+# in background can't prompt for pwd). Moreover, background process will be
+# killed only calling "sudo kill <pid>"
+isSudo=""         # False
+if [[ $1 == "sudo" ]]
+then
+  sudo echo "" >/dev/null
+  isSudo="sudo "  # True
+fi
+
+# Python need "-u" (unbuffered) to output while computing
+if [[ $1 == "python" ]]
+then
+  cmd=${cmd/python/python -u}
+fi
+
+# Execute in background, redirect output(1) and error(2) to <cmd>.log file
+nohup $cmd 1>${cmd// /_}.log 2>&1 &
+echo Job \"$cmd\" started.
+echo - Use \"less ${cmd// /_}.log\" to inspect output/error.
+echo - Use \"${isSudo}kill $!\" to terminate if required.
---
+++ unpackAll.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python -u
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Francesco Lumachi <francesco.lumachi@gmail.com>
+
+import tarfile, os, getopt, sys
+''' Unzippa file multipli a partire dalla directory da cui è chiamato lo script.
+    I files sono decompressi allo stesso livello del file compresso.
+'''
+
+def unpack_GZ(path, tname):
+    tpathname = os.path.join(path, tname)
+    tfile = tarfile.open(tpathname, 'r:gz')
+    members = tfile.getmembers()
+    # Per ogni file, appiattisci la directory di decompressione
+    for m in members:
+        m.name = os.path.basename(m.name)
+    tfile.extractall(path)
+    tfile.close()
+
+# Default
+delete_unpacked = False
+
+# Leggi argparse
+opts, _ = getopt.getopt(sys.argv[1:], "d", ['delete'])
+for opt, arg in opts:
+    if opt=='-d': delete_unpacked = True
+
+# Cerca tutti i files
+for path, _, files in os.walk('.'):
+    # Filtra solo i .gz
+    tarfiles = [f for f in files if os.path.splitext(f)[1] == '.gz']
+    for tname in tarfiles:
+        print 'Unpacking {}'.format(os.path.join(path, tname))
+        unpack_GZ(path, tname)
+        if delete_unpacked:
+            print 'Deleting {}'.format(os.path.join(path, tname))
+            os.remove(os.path.join(path, tname))
+print 'Fatto!'
