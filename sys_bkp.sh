9730c45a63d84f97d3e0d745487280a80758420c
lum4chi <francesco.lumachi@gmail.com>
2016-08-10 21:26:06 +0200

    Removed script backup

--- sys_bkp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#
-# Copyright (C) 2016 Francesco Lumachi <francesco.lumachi@gmail.com>
-
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/data/*" "/backup/*", "/lost+found"} / /backup
